9884063c14349e062ca9d96a4a30b151ed5d4369
markchampion <hieu.tran@bluecoglobal.com>
2021-09-02 16:40:34 +0700

    abc

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService} from 'angularx-social-login';
+import * as SockJS from 'sockjs-client';
+import {Stomp} from '@stomp/stompjs';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +12,8 @@ import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService} from 'ang
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  stompClient: any;
+  form: FormGroup;
 
   constructor(private iconRegistry: MatIconRegistry,
               private sanitizer: DomSanitizer,
@@ -16,9 +21,24 @@ export class LoginComponent implements OnInit {
     iconRegistry.addSvgIcon('login-fb', sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/icons8-facebook.svg'));
     iconRegistry.addSvgIcon('login-gg', sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/icons8-google-150.svg'));
     iconRegistry.addSvgIcon('login-tw', sanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/icons8-twitter.svg'));
+
   }
 
+  username: string;
+
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  _connect() {
+    console.log('Initialize WebSocket Connection');
+    const ws = new SockJS('http://localhost:8080/notify-websocket');
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect({username: this.username}, (frame) => {
+      this.stompClient.subscribe('/user/notify/task', (sdkEvent) => {
+        console.log('response here: ' + sdkEvent);
+      });
+    });
   }
 
   signInWithGoogle(): void {
@@ -37,4 +57,7 @@ export class LoginComponent implements OnInit {
     this.authService.signOut();
   }
 
+  setValue(event): void {
+    this.username = event.target.value;
+  }
 }
